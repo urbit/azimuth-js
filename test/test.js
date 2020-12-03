0c75fc1e62166f2c0f80ade8e32c70d7376ce4e5
Jared Tobin <jared@jtobin.io>
2020-12-04 04:29:32 +0900

    Remove unnecessary async / await keywords.
    ecliptic.{configureKeys, spawn} are not asynchronous, and Promise
    resolver functions should not be likewise.

--- test/test.js
+++ test/test.js
@@ -236,7 +236,7 @@ function main() {
       let tx = ecliptic.spawn(contracts, star1, ac0);
       await sendTransaction(web3, tx, pk0);
 
-      tx = await ecliptic.configureKeys(
+      tx = ecliptic.configureKeys(
              contracts, star1, someBytes32, someBytes32, 1, false);
       await sendTransaction(web3, tx, pk0);
 
@@ -245,7 +245,7 @@ function main() {
       assert.isFalse(await azimuth.isOwner(contracts, star2, ac0));
       assert.isFalse(await azimuth.isActive(contracts, star2));
 
-      tx = await ecliptic.spawn(contracts, star2, ac1);
+      tx = ecliptic.spawn(contracts, star2, ac1);
       await sendTransaction(web3, tx, pk0);
 
       assert.isTrue(await azimuth.isOwner(contracts, star2, ac0));
